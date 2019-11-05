37f3cf88f2f3a255b69bed4f1db560e69a7cb39e
Mustafa Hossaini <mustafahossaini@gmail.com>
2019-11-06 05:05:08 +1300

    basic app structure

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Maz Market',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -20,22 +20,14 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Maz Market'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -44,68 +36,204 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              UserAccountsDrawerHeader(
+                accountName: new Text("Mustafa Hossaini"),
+                accountEmail: new Text("mustafahossainni@gmail.com"),
+                onDetailsPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ProfilePage()),
+                  );
+                },
+                currentAccountPicture: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        "https://www.fakenamegenerator.com/images/sil-female.png")),
+              ),
+              new ListTile(
+                  leading: Icon(Icons.settings),
+                  title: new Text("Setting"),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SettingsPage()),
+                    );
+                  }),
+              new ListTile(
+                  leading: Icon(Icons.help),
+                  title: new Text("Help"),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ProfilePage()),
+                    );
+                  }),
+              new ListTile(
+                  leading: Icon(Icons.history),
+                  title: new Text("Order History "),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => OrderHistoryPage()),
+                    );
+                  }),
+              new ListTile(
+                  leading: Icon(Icons.power_settings_new),
+                  title: new Text(
+                    "Logout",
+                    style:
+                        new TextStyle(color: Colors.redAccent, fontSize: 17.0),
+                  ),
+                  onTap: () {})
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          title: Text(widget.title),
+          bottom: TabBar(
+            tabs: [
+              Tab(text: 'List'),
+              Tab(
+                text: 'Map',
+              ),
+            ],
+          ),
+        ),
+      ), //
+    );
+  }
+}
+
+class SettingsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Settings"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+      body: ListView(children: <Widget>[
+        ListTile(
+          title: Text('Enable Feature'),
+          trailing: Checkbox(
+            onChanged: (val) {},
+            value: false,
+          ),
+          onTap: () {},
+        )
+      ]),
+    );
+  }
+}
+
+class ProductCreateOrEditPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Settings"),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: ListView(children: <Widget>[
+        ListTile(
+          title: Text('Enable Feature'),
+          trailing: Checkbox(
+            onChanged: (val) {},
+            value: false,
+          ),
+          onTap: () {},
+        )
+      ]),
+    );
+  }
+}
+
+class ProductDetailsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Settings"),
+      ),
+      body: ListView(children: <Widget>[
+        ListTile(
+          title: Text('Enable Feature'),
+          trailing: Checkbox(
+            onChanged: (val) {},
+            value: false,
+          ),
+          onTap: () {},
+        )
+      ]),
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Profile"),
+      ),
+      body: ListView(children: <Widget>[
+        ListTile(
+          title: Text('Enable Feature'),
+          trailing: Checkbox(
+            onChanged: (val) {},
+            value: false,
+          ),
+          onTap: () {},
+        )
+      ]),
+    );
+  }
+}
+
+class HelpPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Help"),
+      ),
+      body: ListView(children: <Widget>[
+        ListTile(
+          title: Text('Enable Feature'),
+          trailing: Checkbox(
+            onChanged: (val) {},
+            value: false,
+          ),
+          onTap: () {},
+        )
+      ]),
+    );
+  }
+}
+
+class OrderHistoryPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Orders"),
+      ),
+      body: ListView(children: <Widget>[
+        ListTile(
+          title: Text('Enable Feature'),
+          trailing: Checkbox(
+            onChanged: (val) {},
+            value: false,
+          ),
+          onTap: () {},
+        )
+      ]),
     );
   }
 }
