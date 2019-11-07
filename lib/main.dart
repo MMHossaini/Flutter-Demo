cc8ebdf1e409042279d387424437dadb2142c4ba
Mustafa Hossaini <mustafahossaini@gmail.com>
2019-11-08 06:50:23 +1300

    Add Firestore,  increase minSDKVersion in build.gradle  to 21 from 16 to make firebase and flutter work together

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -106,6 +107,12 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ],
           ),
+        ),
+        body: TabBarView(
+          children: [
+            ProductsList(),
+            Icon(Icons.map),
+          ],
         ),
       ), //
     );
@@ -234,6 +241,32 @@ class OrderHistoryPage extends StatelessWidget {
           onTap: () {},
         )
       ]),
+    );
+  }
+}
+
+class ProductsList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('products').snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) return new Text('Error: ${snapshot.error}');
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return new Text('Loading...');
+          default:
+            return new ListView(
+              children:
+                  snapshot.data.documents.map((DocumentSnapshot document) {
+                return new ListTile(
+                  title: new Text(document['title']),
+                  subtitle: new Text(document['description']),
+                );
+              }).toList(),
+            );
+        }
+      },
     );
   }
 }
