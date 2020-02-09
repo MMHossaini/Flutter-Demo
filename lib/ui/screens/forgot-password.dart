67afca64b65057355ca351fba70824fddc95bbb6
Mustafa <mustafahossaini@gmail.com>
2020-02-09 14:28:51 +1300

    update password page

--- lib/ui/screens/forgot-password.dart
+++ lib/ui/screens/forgot-password.dart
@@ -32,6 +32,10 @@ class ForgotPasswordPageState extends State<ForgotPasswordPage> {
           key: emailForm,
           child: Column(
             children: <Widget>[
+              Icon(
+                Icons.person,
+                size: 200,
+              ),
               Padding(
                 padding: EdgeInsets.only(left: 16.0),
                 child: TextFormField(
@@ -41,58 +45,64 @@ class ForgotPasswordPageState extends State<ForgotPasswordPage> {
                   keyboardType: TextInputType.emailAddress,
                 ),
               ),
-              RaisedButton(
-                child: Text("Send Me Password Reset Email"),
-                onPressed: () async {
-                  if (emailForm.currentState.validate()) {
-                    try {
-                      emailForm.currentState.save();
-                      setState(() {
-                        _showLoading = true;
-                      });
-                      await FirebaseAuth.instance
-                          .sendPasswordResetEmail(email: _email);
+              Padding(
+                padding: EdgeInsets.only(left: 16, right: 16, top: 16),
+                child: SizedBox(
+                  width: double.maxFinite,
+                  child: RaisedButton(
+                    child: Text("Send Me Password Reset Email"),
+                    onPressed: () async {
+                      if (emailForm.currentState.validate()) {
+                        try {
+                          emailForm.currentState.save();
+                          setState(() {
+                            _showLoading = true;
+                          });
+                          await FirebaseAuth.instance
+                              .sendPasswordResetEmail(email: _email);
 
-                      Flushbar(
-                              title: "Email sent",
-                              message:
-                                  "Check your email for password reset instructions",
-                              duration: Duration(seconds: 3))
-                          .show(context);
-                      setState(() {
-                        _showLoading = false;
-                      });
-                    } on PlatformException catch (e) {
-                      switch (e.code) {
-                        case 'ERROR_INVALID_EMAIL ':
                           Flushbar(
-                                  title: "Invalid email",
+                                  title: "Email sent",
                                   message:
-                                      "Computer says that dont like an email address to me",
+                                      "Check your email for password reset instructions",
                                   duration: Duration(seconds: 3))
                               .show(context);
-                          break;
+                          setState(() {
+                            _showLoading = false;
+                          });
+                        } on PlatformException catch (e) {
+                          switch (e.code) {
+                            case 'ERROR_INVALID_EMAIL ':
+                              Flushbar(
+                                      title: "Invalid email",
+                                      message:
+                                          "Computer says that dont like an email address to me",
+                                      duration: Duration(seconds: 3))
+                                  .show(context);
+                              break;
 
-                        case 'ERROR_USER_NOT_FOUND ':
-                          Flushbar(
-                                  title: "Not Registered",
-                                  message:
-                                      "That email address is not registered with us brah",
-                                  duration: Duration(seconds: 3))
-                              .show(context);
-                          break;
+                            case 'ERROR_USER_NOT_FOUND ':
+                              Flushbar(
+                                      title: "Not Registered",
+                                      message:
+                                          "That email address is not registered with us brah",
+                                      duration: Duration(seconds: 3))
+                                  .show(context);
+                              break;
 
-                        default:
-                          Flushbar(
-                                  title: "Something else",
-                                  message:
-                                      "Something else went wrong and ye aint event know it",
-                                  duration: Duration(seconds: 3))
-                              .show(context);
+                            default:
+                              Flushbar(
+                                      title: "Something else",
+                                      message:
+                                          "Something else went wrong and ye aint event know it",
+                                      duration: Duration(seconds: 3))
+                                  .show(context);
+                          }
+                        }
                       }
-                    }
-                  }
-                },
+                    },
+                  ),
+                ),
               ),
             ],
           ),
